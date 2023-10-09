9ceefdccff3586d3255fdab49697e20099765862
David <dchen1561@gmail.com>
2023-10-08 19:16:11 -0700

    Revert "Working commit. please ignore"
    This reverts commit f355d55f9c36a042c63f425515ed354676c5384b.

--- Index.java
+++ Index.java
@@ -11,44 +11,32 @@ import Utilities.FileUtils;
 
 public class Index {
     File index;
-    public Index() throws Exception{
+    public Index() throws IOException{
             Files.createDirectories(Paths.get("objects"));
             index = new File("Index");
-            FileUtils.createFile("Index");
     }
 
     public void writePair(String fileName) throws Exception{
-        // HashMap<String,String> map = new HashMap<String,String>();
+        HashMap<String,String> map = new HashMap<String,String>();
         Blob b = new Blob(fileName);
-        StringBuilder sb = new StringBuilder();
-        b.writeFile();//this can be changed with writeZip().
         if(index.exists()&&index.length()!=0){
-            BufferedReader br = new BufferedReader(new FileReader(index));
-            while(br.ready()){
-                sb.append(br.readLine()+"\n");
-            }
-            br.close();
+        BufferedReader br = new BufferedReader(new FileReader(index));
+        while(br.ready()){
+            String str = br.readLine();
+            str = str.substring(str.indexOf(":")+1);
+            String value = str.substring(0,str.indexOf(":"));
+            String key = str.substring(str.indexOf(":")+1);
+            map.put(key,value);
         }
-            sb.append("blob:"+b.getSHAString()+":"+fileName);
-            FileUtils.writeFile(index.getPath(),sb.toString());
-        
-        // if(index.exists()&&index.length()!=0){
-        // BufferedReader br = new BufferedReader(new FileReader(index));
-        // while(br.ready()){
-        //     String str = br.readLine();
-        //     str = str.substring(str.indexOf(":")+1);
-        //     String value = str.substring(0,str.indexOf(":"));
-        //     String key = str.substring(str.indexOf(":")+1);
-        //     map.put(key,value);
-        // }
-        // br.close();
-        // }
-        // map.put(fileName, b.getSHAString());
-        // FileWriter  fw = new FileWriter(index);
-        // for( String str : map.keySet()){
-        //     fw.append("blob"+":"+map.get(str)+":"+str+"\n");
-        // }
-        // fw.close();
+        br.close();
+        }
+        b.writeFile();//this can be changed with writeZip().
+        map.put(fileName, b.getSHAString());
+        FileWriter  fw = new FileWriter(index);
+        for( String str : map.keySet()){
+            fw.append("blob"+":"+map.get(str)+":"+str+"\n");
+        }
+        fw.close();
     }
 
     public void removePair(String fileName) throws IOException{
@@ -75,26 +63,4 @@ public class Index {
         FileUtils.writeFile("Index", FileUtils.readFile("./objects/"+Paths.get(Tree.addDirectory(path))));
     }
 
-    public void delete(String fileToDelete) throws Exception{
-        BufferedReader br = new BufferedReader(new FileReader("Index"));
-        String add = "";
-        while(br.ready()){
-            add+=br.readLine()+"\n";
-        }
-        add+="*deleted* "+fileToDelete;
-        br.close();
-        FileUtils.writeFile("Index", add);
-    }
-
-    public void edit(String fileToEdit, String edits) throws Exception{
-        FileUtils.writeFile(fileToEdit, edits);
-        BufferedReader br = new BufferedReader(new FileReader("Index"));
-        String add = "";
-        while(br.ready()){
-            add+=br.readLine()+"\n";
-        }
-        add+="*edited* "+fileToEdit;
-        br.close();
-        FileUtils.writeFile("Index", add);
-    }
 }
